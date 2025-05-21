ca82e9cc4a1968ed21bf98e6d601390c911a2fbf
nnajeh <38373885+nnajeh@users.noreply.github.com>
2025-05-21 16:20:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ train_labels_file = './ml_exercise_therapanacea/label_train.txt'
 
 
 if __name__ == '__main__':
+
+    # path to train and valid datasets
+    train_img_dir = './ml_exercise_therapanacea/train_img/'
+    val_img_dir = './ml_exercise_therapanacea/val_img/'
+    train_labels_file = './ml_exercise_therapanacea/label_train.txt'
+
     
     train_loader, val_loader = get_data_loaders(train_img_dir, train_labels_file, val_img_dir)
 
