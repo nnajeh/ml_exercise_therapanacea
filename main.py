80f68de336691c5c2e2c0b314f667300a1226893
nnajeh <38373885+nnajeh@users.noreply.github.com>
2025-05-21 16:17:54 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from  bib import *
+from models import *
+from data.data_loader import get_data_loaders
+
+
+
+# path to train and valid datasets
+train_img_dir = './ml_exercise_therapanacea/train_img/'
+val_img_dir = './ml_exercise_therapanacea/val_img/'
+train_labels_file = './ml_exercise_therapanacea/label_train.txt'
+
+
+if __name__ == '__main__':
+    
+    train_loader, val_loader = get_data_loaders(train_img_dir, train_labels_file, val_img_dir)
+
+    train_model(train_loader)
+  
+    predict(val_loader)
