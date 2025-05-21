6349a9412a8564cce911acfcc9ba419dcb3c015f
nnajeh <38373885+nnajeh@users.noreply.github.com>
2025-05-21 16:16:20 +0200

    Create bib.py

---
+++ bib.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+
+
